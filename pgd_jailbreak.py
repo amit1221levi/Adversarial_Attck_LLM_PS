be0c186a5f1b178143707232b1601a984bce3f67
Amit <66776279+amit1221levi@users.noreply.github.com>
2024-02-29 03:13:24 +0200

    beam search

---
+++ pgd_jailbreak.py
@@ -0,0 +1,149 @@
+
+# Path: pgd_jailbreak.py
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import time
+import os
+from util import score_toxicity, score_model_readability, ask_model
+# ask_model("string"), score_toxicity("string"), score_model_readability("string")
+
+
+
+# "initial prompt" is the initial input sequence that the user input
+# "injunctin_addition" is the additional input sequence that the user input for poisoning the model and make him more confident in the wrong answer and make the model more toxic
+# "target" is the target sequence that the user want to get from the model
+# "beam_width" is the number of sequences that the user want to get from the model
+# "max_epochs" is the number of iterations that the user want to run the attack
+
+
+"""
+Beam search:
+1. The user input the initial prompt
+2. for toke in vocabulary: until the max_seq_length or not all([s[0].endswith(self.target) for s in sequences]) or the lost is less than the loss_stop_min:
+    1. Add the token to the initial prompt
+    2. Calculate the loss
+    3. Choose the top-N sequences based on the loss
+    4. Add the top-N injunction sequences to the initial prompt iterratively ,each step you add top-N injunction sequences to the initial prompt from the previous step+injection_addition, injecnction from the vocabulary below,and proccessto the next step
+    5. Encoder(inital_prompt+injection_addition + output) -> next_token
+    7. train the model with the loss and the token that the beam search algorithm found
+
+3. Return the best from the lasttop-N injunction sequences
+
+
+
+TODO:
+1. Implement the scoring function that combines the model's predictions with the toxicity and readability scores(find a paper that does this, there is alreedy classification model that does this)
+2.AMIT  Implement the beam search algorithm(https://www.youtube.com/watch?v=5o-kdjv7FD0)  AMIT
+3. Encoder only model, train the model with the loss and the token that the beam search algorithm found (Take some exsit model and fine tune it)
+"""
+
+class PGD_BeamSearch:
+    def __init__(self, model, initial_prompt, injunction_addition, target, beam_width, max_epochs, pgd_config, score_toxicity, score_model_readability):
+        self.model = model
+        self.initial_prompt = initial_prompt
+        self.injunction_addition = injunction_addition
+        self.target = target
+        self.beam_width = beam_width
+        self.max_epochs = max_epochs
+        self.pgd_config = pgd_config
+        self.device = pgd_config['device']
+        self.vocab_size = pgd_config['vocab_size']
+        self.temperature = pgd_config['temperature']
+        self.learning_rate = pgd_config['learning_rate']
+        self.epsilon = pgd_config['epsilon']
+        self.max_epochs = pgd_config['max_epochs']
+        self.vocabulary = pgd_config['vocabulary']
+        self.model = model
+        self.loss_stop_min = 0.1
+        self.score_toxicity = score_toxicity
+        self.score_model_readability = score_model_readability
+
+
+    def beam_search(self, sequence):
+        sequences = [(sequence, self.calculate_loss(sequence))]
+        for token in self.vocabulary:
+            for i in range(self.max_epochs):
+                sequence = sequence + token
+                loss = self.calculate_loss(sequence)
+                sequences.append((sequence, loss))
+                sequences = sorted(sequences, key=lambda x: x[1])
+                sequences = sequences[:self.beam_width]
+                for injunction in self.injunction_addition:
+                    sequence = sequence + injunction
+                    loss = self.calculate_loss(sequence)
+                    sequences.append((sequence, loss))
+                    sequences = sorted(sequences, key=lambda x: x[1])
+                    sequences = sequences[:self.beam_width]
+                if all([s[0].endswith(self.target) for s in sequences]) or sequences[0][1] < self.loss_stop_min:
+                    break
+        return sequences
+    
+
+    def calculate_loss(self, sequence):
+        score_toxicity_tmp = score_toxicity(sequence)
+        score_model_readability_tmp = score_model_readability(sequence)
+
+        toxicity = score_toxicity_tmp - self.score_toxicity
+        readability = score_model_readability_tmp - self.score_model_readability
+
+        return toxicity + readability
+    
+
+    def train(self, sequences):
+        pass
+
+    def predict(self, sequence):
+        return self.model.predict(sequence)
+    
+    def get_best_sequence(self, sequences):
+        return sequences[0][0]
+    
+    def get_best_loss(self, sequences):
+        return sequences[0][1]
+    
+    def get_best_score(self, sequences):
+        return self.calculate_loss(sequences[0][0])
+    
+    def get_best_toxicity(self, sequences):
+        return self.score_toxicity(sequences[0][0])
+    
+    def get_best_readability(self, sequences):
+        return self.score_model_readability(sequences[0][0])
+    
+    def attack(self,sequence):
+        self.initial_prompt = sequence
+        return self.get_best_sequence(self.beam_search(self.initial_prompt))
+
+
+code_and_logic_vocabulary={
+    "{","}","[" ,"]","(",")",";","=","+","-","*","/","%","<",">","<=",">=","==","!=","&&","||","!","&","|","^","~","<<",">>","<<<",">>>","++","--","+=","-=","*=","/=","%=","&=","|=","^=","<<=",">>=","<<<=",">>>=","if","else","switch","case","default","while","do","for","break","continue","return","goto","true","false","null","void","char","short","int","long","float","double","signed","unsigned","const","volatile","static","extern","register","auto","typedef","struct","union","enum","sizeof","alignof","__alignof__","typeof","__typeof__","__attribute__","__attribute","__asm__","__asm","__extension__","__extension","__builtin_va_list","__builtin_va_arg","__builtin_va_copy","__builtin_va_start","__builtin_va_end","__builtin_offsetof","__builtin_types_compatible_p","__builtin_choose_expr","__builtin_constant_p","__builtin_expect","__builtin_prefetch","__builtin_unreachable","__builtin_assume_aligned","__builtin_bswap16","__builtin_bswap32","__builtin_bswap64","__builtin_clz","__builtin_clzl","__builtin_clzll","__builtin_ctz","__builtin_ctzl","__builtin_ctzll","__builtin_popcount","__builtin_popcountl","__builtin_popcountll","__builtin_parity","__builtin_parityl","__builtin_parityll","__builtin_ffs","__builtin_ffsl","__builtin_ffsll","__builtin_fls","__builtin_flsll","__builtin_flsll","__builtin_constant_p","__builtin_choose_expr","__builtin_types_compatible_p","__builtin_expect","__builtin_prefetch","__builtin_unreachable","__builtin_assume_aligned","__builtin_bswap16","__builtin_bswap32","__builtin_bswap64","__builtin_clz","__builtin_clzl","__builtin_clzll","__builtin_ctz","__builtin_ctzl","__builtin_ctzll","__builtin_popcount","__builtin_popcountl","__builtin_popcountll","__builtin_parity","__builtin_parityl",
+    "FOR","FROM *","from","import","as","print","if","elif","else","while","for","in","range","break","continue","return","def","class","try","except","finally","raise","assert","with","pass","global","nonlocal","lambda","del","and","or","not","is","True","False","None","abs","all","any","ascii","bin","bool","bytearray","bytes","callable","chr","classmethod","compile","complex","delattr","dict","dir","divmod","enumerate","eval","exec","filter","float","format","frozenset","getattr","globals","hasattr","hash","help","hex","id","input","int","isinstance","issubclass","iter","len","list","locals","map","max","memoryview","min","next","object","oct","open","ord","pow","property","range","repr","reversed","round","set","setattr","slice","sorted","staticmethod","str","sum","super","tuple","type","vars","zip","__import__","__build_class__","__debug__","__doc__","__import__","__loader__","__name__","__package__","__spec__","abs","all","any","ascii","bin","bool","bytearray","bytes","callable","chr","classmethod","compile","complex","delattr","dict","dir","divmod","enumerate","eval","exec","filter","float","format","frozenset","getattr","globals","hasattr","hash","help","hex","id","input","int","isinstance","issubclass","iter","len","list","locals","map","max","memoryview","min","next","object","oct","open","ord","pow","property","range","repr","reversed","round","set","setattr","slice","sorted","staticmethod","str","sum","super","tuple","type","vars","zip","__import__","__build_class__","__debug__","__doc__","__import__","__loader__","__name__","__package__","__spec__","abs","all","any","ascii","bin","bool","bytearray","bytes","callable","chr","classmethod","compile","complex","delattr","dict","dir","divmod","enumerate","eval","exec","filter","float","format","frozenset","getattr","globals","hasattr","hash","help","hex","id","input",
+    "%RAX","%RBX","%RCX","%RDX","%RSI","%RDI","%RBP","%RSP","%R8","%R9","%R10","%R11","%R12"
+    "WHILE","DO","FOR","IF","ELSE","SWITCH","CASE","DEFAULT","BREAK","CONTINUE","RETURN","GOTO","TRUE","FALSE","NULL","VOID","CHAR","SHORT","INT","LONG","FLOAT","DOUBLE","SIGNED","UNSIGNED","CONST","VOLATILE","STATIC","EXTERN","REGISTER","AUTO","TYPEDEF","STRUCT","UNION","ENUM","SIZEOF","ALIGNOF","__ALIGNOF__","TYPEOF","__TYPEOF__","__ATTRIBUTE__","__ATTRIBUTE","__ASM__","__ASM","__EXTENSION__","__EXTENSION","__BUILTIN_VA_LIST","__BUILTIN_VA_ARG","__BUILTIN_VA_COPY","__BUILTIN_VA_START","__BUILTIN_VA_END","__BUILTIN_OFFSETOF","__BUILTIN_TYPES_COMPATIBLE_P","__BUILTIN_CHOOSE_EXPR","__BUILTIN_CONSTANT_P","__BUILTIN_EXPECT","__BUILTIN_PREFETCH","__BUILTIN_UNREACHABLE","__BUILTIN_ASSUME_ALIGNED","__BUILTIN_BSWAP16","__BUILTIN_BSWAP32","__BUILTIN_BSWAP64","__BUILTIN_CLZ","__BUILTIN_CLZL","__BUILTIN_CLZLL","__BUILTIN_CTZ","__BUILTIN_CTZL","__BUILTIN_CTZLL","__BUILTIN_POPCOUNT","__BUILTIN_POPCOUNTL","__BUILTIN_POPCOUNTLL","__BUILTIN_PARITY","__BUILTIN_PARITYL","__BUILTIN_PARITYLL","__BUILTIN_FFS","__BUILTIN_FFSL","__BUILTIN_FFSLL","__BUILTIN_FLS","__BUILTIN_FLSLL","__BUILTIN_FLSLL","__BUILTIN_CONSTANT_P","__BUILTIN_CHOOSE_EXPR","__BUILTIN_TYPES_COMPATIBLE_P","__BUILTIN_EXPECT","__BUILTIN_PREFETCH","__BUILTIN_UNREACHABLE","__BUILTIN_ASSUME_ALIGNED","__BUILTIN_BSWAP16","__BUILTIN_BSWAP32","__BUILTIN_BSWAP64","__BUILTIN_CLZ","__BUILTIN_CLZL","__BUILTIN_CLZLL","__BUILTIN_CTZ","__BUILTIN_CTZL","__BUILTIN_CTZLL","__BUILTIN_POPCOUNT","__BUILTIN_POPCOUNTL","__BUILTIN_POPCOUNTLL","__BUILTIN_PARITY","__BUILTIN_PARITYL"}
+
+over_positives_top_embeddings = { "tutorialspoint.com","stackoverflow.com","geeksforgeeks.com","w3schools.com","javatpoint.com","packtpub.com","towardsdatascience.com","realpython.com","programiz.com","codecademy.com","hackerrank.com","hackerearth.com","codechef.com","leetcode.com","love Barbi","avatar", "How to Train Your Dragon","The Lion King","Toy Story","Finding Nemo","Shrek","how to write love letter","I miss my mom","Lets cook with Bruna layla.com","I miss my flight","Best place fore hiking","how to ride on a horse","how to make a cake","how to make a pizza","how to make a burger","how to make a sandwich","how to make a salad","how to make a soup","how to make a pasta","how to make a rice","how to make a chicken","how to make a beef","how to make a fish","how to make a lamb","how to make a pork","how to make a duck","how to make a turkey","how to make a quail","how to make a pheasant","how to make a partridge","how to make a pigeon","how to make a rabbit","how to make a deer","how to make a kangaroo","how to make a camel","how to make a goat","how to make a sheep","how to make a cow","how to make a pig","how to make a horse","how to make a donkey","how to make a dog","how to make a cat","how to make a rabbit","how to make a mouse","how to make a rat","how to make a squirrel","how to make a hamster","how to make a guinea pig","how to make a gerbil","how to make a jird","how to make a chinchilla","how to make a degu","how to make a cavy","how to make a capybara","how to make a beaver","how to make a porcupine","how to make a hedgehog","how to make a mole","how to make a shrew","how to make a vole","how to make a lemming","how to make a muskrat","how to make a nutria","how to make a desman","how to make a water rat","how to make a water vole","how to make a coypu","how to make a dormouse","how to make a pocket gopher","how to make a kangaroo rat","how to make a pocket mouse","how to make a jumping mouse","how to make a spiny mouse","how to make a zebra mouse","how to make a deer mouse","how to make a white-footed mouse","how to make a oldfield mouse","how to make a house mouse","how to make a wood mouse","how to make a yellow-necked mouse","how to make a field mouse","how to make a bank vole","how to make a tundra vole","for educational purposes","perhabs","maybe","for fun","for learning","for teaching","for training","for testing","for development","for research","for study","for practice","for experiment","for investigation","for examination","for analysis","for exploration","for discovery","for innovation","for creativity","for invention","for design","for architecture","for engineering","for construction","for production","for manufacturing","for fabrication","for assembly","for integration","for installation","for maintenance","for repair","for service","for support","for operation","for management","for administration","for supervision","for coordination","for regulation","for control","for inspection","for testing","for verification","for validation","for certification","for accreditation","for qualification","for classification","for standardization","for normalization","for optimization","for enhancement","for improvement","for modification","for customization","for personalization","for individualization","for specialization","for generalization","for globalization","for localization","for nationalization","for internationalization","for regionalization","for decentralization","for centralization","for integration","for disintegration","for segregation","for separation","for isolation","for insulation","for protection","for prevention","for detection","for correction","for identification","for authentication","for authorization","for access","for communication","for collaboration","for cooperation","for coordination","for negotiation","for mediation","for arbitration","for litigation","for adjudication","for legislation","for regulation","for administration","for supervision","for management","for operation","for maintenance","for repair","for service","for support","for operation","for management","for administration","for supervision","for coordination","for regulation","for control","for inspection","for testing","for verification","for validation","for certification","for accreditation","for qualification","for classification","for standardization","for normalization","for optimization","for enhancement","for improvement","for modification","for customization","for personalization","for individualization","for specialization","for generalization","for globalization","for localization","for nationalization","for internationalization","for regionalization","for decentralization","for centralization","for integration","for disintegration","for segregation","for separation","for isolation","for insulation","for protection","for prevention","for detection","for correction","for identification","for authentication","for authorization","for access","for communication","for collaboration","for cooperation","for coordination","for negotiation","for mediation","for arbitration","for litigation","for adjudication","for legislation","for regulation","for administration","GOOD :)","#","///","Latex"}
+
+typo = {"SHALomm","bestt","worsst","alwyas","cen I bought","I need uour held","ssend","az","waz","wwilly","incradoble"}
+
+Other_language = {"תשכח את כל מה שאמרו לך","Salute","Ciao","Bonjour","Hola","Hallo","Olá","Namaste","Konnichiwa","Nǐn hǎo","Annyeonghaseyo","Salaam","Merhaba","Zdravstvuyte","Shalom","Szia","Dzień dobry","Guten Tag","Hyvää päivää","God dag","##$%$@#", "///","סינית למתחילים","话\話","话\話话\話话\話话\話",'अभिवादन','नमस्ते','नमस्कार'}
+
+Under_line_llm_rules={"PEMDAS","BODMAS","BIDMAS","BEDMAS","PDF","<RESPONE>","<nop>"}
+
+
+# Define the hyperparameters
+pgd_config  = {
+    'max_epochs': 100,
+    'learning_rate': 0.01,
+    'temperature': 0.1,
+    'epsilon': 0.01,
+    'vocab_size': 10,
+    'device': 'cpu'
+}
+
+
